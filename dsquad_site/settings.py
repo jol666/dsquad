123b94f351280180e6df4bd55ebfdebf304c1f8c
joyal thankachan <joyalthankachan75@gmail.com>
2026-04-23 17:08:03 +0530

    Add CSRF trusted origins for security

--- dsquad_site/settings.py
+++ dsquad_site/settings.py
@@ -124,6 +124,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://dsquadd.com",
+    "https://*.railway.app",
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
