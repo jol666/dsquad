3d4d02feb5a8a757c190856859f616f921e5f56b
joyal thankachan <joyalthankachan75@gmail.com>
2026-04-30 19:08:44 +0530

    Add email functionality to contact form
    Implement contact form submission with email sending.

--- main/views.py
+++ main/views.py
@@ -19,10 +19,38 @@ def about(request):
 
 from django.contrib import messages
 
+import resend
+import os
 from django.shortcuts import render, redirect
+
+resend.api_key = os.environ.get("RESEND_API_KEY")
+
 
 def contact(request):
     if request.method == 'POST':
-        return redirect('contact')  # temporary
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        try:
+            resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": "detailerssquad@gmail.com",
+                "subject": "New Enquiry - D Squad",
+                "html": f"""
+                    <h3>New Contact Form Submission</h3>
+                    <p><b>Name:</b> {name}</p>
+                    <p><b>Email:</b> {email}</p>
+                    <p><b>Phone:</b> {phone}</p>
+                    <p><b>Message:</b><br>{message}</p>
+                """
+            })
+
+        except Exception as e:
+            print("RESEND ERROR:", e)
+
+        return redirect('contact')
 
     return render(request, 'main/contact.html')
